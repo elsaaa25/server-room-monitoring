cefadf79c432453cebbcea3404ecfd505ea953af
Elsa <elsaadevita@gmail.com>
2026-07-24 16:09:53 +0700

    Update: Dashboard

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1318,7 +1318,7 @@ export function Dashboard() {
 
                   <Metric
                     icon={Zap}
-                    label="Tegangan Server AC"
+                    label="Tegangan"
                     value={
                       readingL4?.voltage !==
                         null &&
